d3c04fd1194f8d24e4008aaa7184c2d77e65df48
ChriZiegler <ChriZiegler@gmail.com>
2017-02-23 10:28:24 -0600

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cloud_whale
 E-commerce website for crafts
+
+Currently hosted at: https://chriziegler.github.io/cloud_whale/
--- controllers/myStoreController.js
+++ controllers/myStoreController.js
@@ -1,6 +1,7 @@
 myStore.controller("myStoreController", function($http){
-  vm = this
-  vm.products = $http.get("data/products.json").then(
+  vm = this;
+  vm.products = []; //temporarily bind products to avoid error
+  $http.get("data/products.json").then(
     function(response){
       vm.products = response.data;
     }, function(error){
